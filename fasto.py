a34712518a203a987c651b645dca733773de7abe
Rob Pellowski <23526472+RPellowski@users.noreply.github.com>
2021-11-06 21:19:50 -0400

    Add line detection

--- fasto.py
+++ fasto.py
@@ -1,5 +1,17 @@
 import cv2
 import time
+import math
+NS_PER_S=1000000000
+ESC=27
+
+def find_square(oframe):
+    frame = cv2.cvtColor(oframe, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(frame, threshold1=50, threshold2=200, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, 1, math.pi / 180, threshold=80, minLineLength=10, maxLineGap=10);
+    for line in lines[0]:
+        pt1 = (line[0], line[1])
+        pt2 = (line[2], line[3])
+        cv2.line(oframe, pt1, pt2, (0, 0, 255), 3)
 
 def show_live():
     t0=time.time()
@@ -16,13 +28,23 @@ def show_live():
     else:
         rval = False
 
+    t1=time.time_ns()
+    frames=0
+    s='0'
     while rval:
+        frames+=1
+        dt=time.time_ns()-t1
+        if (dt > NS_PER_S):
+            s=str(frames)
+            t1=time.time_ns()
+            frames=0
+        cv2.putText(frame, s, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
+        find_square(frame)
         cv2.imshow("camera", frame)
         rval, frame = cam.read()
-        cv2.putText(frame, "My text", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
         key = cv2.waitKey(20)
-        if key == 27: # exit on ESC
+        if key == ESC: # exit on ESC
             break
 
     cam.release()
