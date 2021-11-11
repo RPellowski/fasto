f6c546fe86e0aad413ad69a21450b2144b9f4859
Rob Pellowski <23526472+RPellowski@users.noreply.github.com>
2021-11-11 07:57:52 -0500

    Add bounding box reduction test

--- fasto.py
+++ fasto.py
@@ -9,8 +9,73 @@ ESC=27
 layerNames = [
 	"feature_fusion/Conv_7/Sigmoid",
 	"feature_fusion/concat_3"]
-net = cv2.dnn.readNet(r"c:\Documents and Settings\rpell\Coding\Fasto\EAST-Detector-for-text-detection-using-OpenCV\frozen_east_text_detection.pb")
+#net = cv2.dnn.readNet(r"c:\Documents and Settings\rpell\Coding\Fasto\EAST-Detector-for-text-detection-using-OpenCV\frozen_east_text_detection.pb")
 
+# Reference
+# https://github.com/PyImageSearch/imutils/blob/master/imutils/object_detection.py#L4
+def non_max_suppression(boxes, probs=None, overlapThresh=0.3):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+
+	# if the bounding boxes are integers, convert them to floats -- this
+	# is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	# initialize the list of picked indexes
+	pick = []
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 2]
+	y2 = boxes[:, 3]
+
+	# compute the area of the bounding boxes and grab the indexes to sort
+	# (in the case that no probabilities are provided, simply sort on the
+	# bottom-left y-coordinate)
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = y2
+
+	# if probabilities are provided, sort on them instead
+	if probs is not None:
+		idxs = probs
+
+	# sort the indexes
+	idxs = np.argsort(idxs)
+
+	# keep looping while some indexes still remain in the indexes list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the index value
+		# to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the largest (x, y) coordinates for the start of the bounding
+		# box and the smallest (x, y) coordinates for the end of the bounding
+		# box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+
+		# delete all indexes from the index list that have overlap greater
+		# than the provided overlap threshold
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+
+	# return only the bounding boxes that were picked
+	return boxes[pick].astype("int")
+    
 def find_square(oframe):
     frame = cv2.cvtColor(oframe, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(frame, threshold1=50, threshold2=200, apertureSize=3)
@@ -142,5 +207,33 @@ def foo():
     cam.release()
     cv2.destroyWindow("camera")
 
+def bounding_reduction():
+    height=480
+    width=640
+    cv2.namedWindow("camera")
+    frame = np.zeros((height, width, 3), np.uint8)
+    rects=np.load(r'c:\Documents and Settings\rpell\Coding\Fasto\rects.data.npy')
+    print(rects.shape)
+    for i, (startX, startY, endX, endY) in enumerate(rects):
+        print(i, startX,startY,endX,endY)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+
+    rects=non_max_suppression(rects)
+    print("----------")
+    print(rects.shape)
+    for i, (startX, startY, endX, endY) in enumerate(rects):
+        print(i, startX,startY,endX,endY)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+    cv2.imshow("camera", frame)
+    while True:
+        key = cv2.waitKey(20)
+        if key == ESC: # exit on ESC
+            break
+
+    cv2.destroyWindow("camera")
+
+
 if __name__ == '__main__':
-    show_live()
+    #show_live()
+    bounding_reduction()
